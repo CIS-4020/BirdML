295fa68059ec8b7a51fda22dea369222a8bf9e43
Outry08 <outryvball04@gmail.com>
2025-11-11 17:23:59 -0500

    Adjusted Image Processing to sequentially index without skipping

--- scripts/process_raw_data.py
+++ scripts/process_raw_data.py
@@ -5,63 +5,69 @@ import shutil
 firstClass = 295
 boxDimension = 224
 
-with open("../raw_data/classes.txt", "r") as file:
-    with open("../processed_data/classes.txt", "w") as newFile:
 
-        content = file.read()
-
-        lines = content.strip().split("\n")
-
-        for line in lines:
-            classNum, className = line.split(" ", maxsplit=1)
-
-            newClassNum = int(classNum) - firstClass
-
-            if newClassNum > -1:
-                print(className)
-                newFile.write(str(newClassNum) + " " + className + "\n")
                 
 rawImgPath = "../raw_data/images"
 newImgPath = "../processed_data/images"
 if not os.path.exists(f"{newImgPath}"): os.mkdir(f"{newImgPath}")
 
-for folder in os.listdir(rawImgPath):
+nextClass = 0
 
-    
-    newFolder = str(int(folder)-int(firstClass))
+open("../processed_data/classes.txt", "w").close()
 
-    if os.path.exists(f"{newImgPath}/{newFolder}"):
-        shutil.rmtree(f"{newImgPath}/{newFolder}")
-    
-    os.mkdir(f"{newImgPath}/{newFolder}")
+with open("../raw_data/classes.txt", "r") as rawClassFile:
+    content = rawClassFile.read()
+    lines = content.strip().split("\n")
+    with open("../processed_data/classes.txt", "a") as newClassFile:
+        for i in range(295, 1011):
 
-    for file in os.listdir(f"{rawImgPath}/{folder}"):
+            folderName = str(i)
+            if i < 1000:
+                folderName = "0" + folderName
 
-        img = cv2.imread(f"{rawImgPath}/{folder}/{file}")
+            if not os.path.exists(f"{rawImgPath}/{folderName}"):
+                continue
+            # newFolder = str(int(folder)-int(firstClass))
 
-        baseHeight, baseWidth, baseChannels = img.shape
+            if os.path.exists(f"{newImgPath}/{nextClass}"):
+                shutil.rmtree(f"{newImgPath}/{nextClass}")
+            
+            os.mkdir(f"{newImgPath}/{nextClass}")
 
-        if baseWidth > baseHeight:
-            ratio = baseHeight/baseWidth
-            width = boxDimension
-            height = int(boxDimension * ratio)
-        else:
-            ratio = baseWidth/baseHeight
-            height = boxDimension
-            width = int(boxDimension * ratio)
+            # print(lines)
+            classNum, className = lines[int(folderName)].split(" ", maxsplit=1)
+            newClassFile.write(str(nextClass) + " " + className + "\n")
+            newClassFile.flush()
 
-        resized_img = cv2.resize(img, (width, height))
+            for imgFile in os.listdir(f"{rawImgPath}/{folderName}"):
 
-        top = (boxDimension - height) // 2
-        bottom = boxDimension - height - top
-        left = (boxDimension - width) // 2
-        right = boxDimension - width - left
+                img = cv2.imread(f"{rawImgPath}/{folderName}/{imgFile}")
 
-        squared_img = cv2.copyMakeBorder(
-            resized_img,
-            top, bottom, left, right,
-            cv2.BORDER_CONSTANT,
-            value=[0, 0, 0]  # Black padding
-        )
+                baseHeight, baseWidth, baseChannels = img.shape
 
-        cv2.imwrite(f"{newImgPath}/{newFolder}/{file}", squared_img)
+                if baseWidth > baseHeight:
+                    ratio = baseHeight/baseWidth
+                    width = boxDimension
+                    height = int(boxDimension * ratio)
+                else:
+                    ratio = baseWidth/baseHeight
+                    height = boxDimension
+                    width = int(boxDimension * ratio)
+
+                resized_img = cv2.resize(img, (width, height))
+
+                top = (boxDimension - height) // 2
+                bottom = boxDimension - height - top
+                left = (boxDimension - width) // 2
+                right = boxDimension - width - left
+
+                squared_img = cv2.copyMakeBorder(
+                    resized_img,
+                    top, bottom, left, right,
+                    cv2.BORDER_CONSTANT,
+                    value=[0, 0, 0]  # Black padding
+                )
+
+                cv2.imwrite(f"{newImgPath}/{nextClass}/{imgFile}", squared_img)
+
+            nextClass+=1
